a0c516d5516e370aeb31c9a6173c315cb8f5a2a4
196918327 <196918327@qq.com>
2020-08-09 04:28:04 +0800

    new

--- install.sh
+++ install.sh
@@ -3,27 +3,30 @@
 #cs
 #2020年8月4日22:51:48
 #############################
+DIR="/usr/local/openresty"
 
 function installop (){
-useradd www -s /sbin/nologin
 yum install -y yum-utils
 yum-config-manager --add-repo https://openresty.org/package/centos/openresty.repo
 yum clean all && yum makecache
 yum install -y openresty
 }
 function setopenresty () {
-mkdir -p /usr/local/openresty/nginx/conf.d/
+mkdir -p $DIR/nginx/conf.d/
 mkdir -p /var/log/nginx/cache
-chown www.www -R /var/log/nginx/cache
-\cp -b nginx.conf  /usr/local/openresty/nginx/conf/
-\cp -b default.conf  /usr/local/openresty/nginx/conf.d/
-mkdir -p /usr/local/openresty/nginx/conf/waf/
-\cp -r * /usr/local/openresty/nginx/conf/waf/
-ln -snf /usr/local/openresty/lualib/resty /usr/local/openresty/nginx/conf/waf/resty
+chown nobody.nobody -R /var/log/nginx
+\cp -b nginx.conf  $DIR/nginx/conf/
+if [ ! -f $DIR/nginx/conf.d/default.conf ];then
+cp default.conf  $DIR/nginx/conf.d/
+else
+fi
+mkdir -p $DIR/nginx/conf/waf/
+\cp -r * $DIR/nginx/conf/waf/
+ln -snf $DIR/lualib/resty $DIR/nginx/conf/waf/resty
 }
 
-if [ -d /usr/local/openresty/nginx ];then
-read -p "/usr/local/openresty exists,continue install ? (y/n)" anzhuang
+if [ -d $DIR/nginx ];then
+read -p "$DIR exists,continue install ? (y/n)" anzhuang
 	if [[ $anzhuang == 'y' ]];then
 	setopenresty
 	else

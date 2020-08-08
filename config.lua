38d8bf1ee072b08b64e1b3828a9ddbd061a11b94
196918327 <196918327@qq.com>
2020-08-08 12:56:42 +0800

    new1

--- config.lua
+++ config.lua
@@ -12,9 +12,6 @@ ipBlocklist={"1.0.0.1"}
 CCDeny="on"
 CCrate="100/60"
 html=[[
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>网站防火墙</title></head>
-<body style=" background-color: #444; padding-top: 15rem;"><div style="margin: auto; width:90%;height:15rem; color:#fff; font-size:3rem; background:#6bb3f6;padding-left: 1rem;">
-网站防火墙<hr>您的请求带有不合法参数，已被拦截并记录!</div></body>
-</html>
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<h2 align="center">Encounter Firewall</h2>
 ]]
--- install.sh
+++ install.sh
@@ -3,24 +3,36 @@
 #cs
 #2020年8月4日22:51:48
 #############################
-if [ -d /usr/local/openresty ];then
-echo "/usr/local/openresty exists!!"
-exit
-fi
+
+function installop (){
 useradd www -s /sbin/nologin
 yum install -y yum-utils
 yum-config-manager --add-repo https://openresty.org/package/centos/openresty.repo
 yum clean all && yum makecache
 yum install -y openresty
-
+}
+function setopenresty () {
 mkdir -p /usr/local/openresty/nginx/conf.d/
 mkdir -p /var/log/nginx/cache
 chown www.www -R /var/log/nginx/cache
-\cp nginx.conf  /usr/local/openresty/nginx/conf/
-\cp default.conf  /usr/local/openresty/nginx/conf.d/
+\cp -b nginx.conf  /usr/local/openresty/nginx/conf/
+\cp -b default.conf  /usr/local/openresty/nginx/conf.d/
 mkdir -p /usr/local/openresty/nginx/conf/waf/
 \cp -r * /usr/local/openresty/nginx/conf/waf/
 ln -snf /usr/local/openresty/lualib/resty /usr/local/openresty/nginx/conf/waf/resty
+}
+
+if [ -d /usr/local/openresty/nginx ];then
+read -p "/usr/local/openresty exists,continue install ? (y/n)" anzhuang
+	if [[ $anzhuang == 'y' ]];then
+	setopenresty
+	else
+	exit
+	fi
+else
+installop
+setopenresty
+fi
 
 if [ $? -eq 0 ];then
 echo "*** Install successfully, start it with \"systemctl start openresty\""
